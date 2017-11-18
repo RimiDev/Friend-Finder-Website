d5d08937afa52d6c114f1bb1a312461ddaec3a73
vagrant <vagrant@homestead>
2017-11-18 16:48:03 +0000

    Added the courses table

---
+++ database/migrations/2017_11_18_164603_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->string('program');
+            $table->string('courseNumber')->unique();
+			$table->string('title');
+			$table->string('teacher');
+			$table->timestamps('startTime');
+			$table->timestamps('endTime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
