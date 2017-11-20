3fb76e2b09c4e785a1bfab3359120ba609b1376e
vagrant <maximelacassedeveloper@gmail.com>
2017-11-20 11:26:59 +0000

    Laravel setup + migrations completed according to what we discussed Friday, Nov 17.

--- database/migrations/2017_11_20_112313_create_user_course_table.php
+++ database/migrations/2017_11_20_112313_create_user_course_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserCourseTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->string('firstName');
-			$table->string('lastName');
+        Schema::create('user_course', function (Blueprint $table) {
             $table->string('email')->unique();
-            $table->string('password');
-			$table->rememberToken();
-            $table->timestamps();
-			$table->string('program');
+			$table->string('courseNumber');
         });
     }
 
@@ -31,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_course');
     }
 }
