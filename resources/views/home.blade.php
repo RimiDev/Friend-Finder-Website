9b5de96d93fe27716537638305f8f00e339b0a1f
vagrant <maximelacassedeveloper@gmail.com>
2017-11-22 14:52:04 +0000

    Adding some styles to homepage.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,35 @@
 
 @extends('layouts.app')
 
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+
+        .panel-body1 {
+          text-align:left;
+
+        }
+        .panel-body2 {
+          text-align:right;
+          float: right;
+        }
+
+        </style>
+
+
+
+<body>
 @section('content')
 <div class="container">
   <h1> Welcome to Dawson Friend Finder 2017 </h1>
@@ -16,17 +44,38 @@
                 <div class="panel-heading">
                 </div>
 
-                <div class="panel-body">
+                <div class="panel-body1">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif
 
+                    <p> blah </p>
+
 
                 </div>
+
+                <div class="panel-body2">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <p> blah </p>
+
+
+                </div>
+
+
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+</body>
+
+
+</html>
