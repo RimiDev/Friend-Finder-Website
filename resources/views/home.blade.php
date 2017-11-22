4949444f4adec0c3e9876c78793cdacf966f3060
aciotola <alessandromciotola@gmail.com>
2017-11-22 11:28:10 -0500

    Add styling to homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,78 +1,76 @@
-
 @extends('layouts.app')
 
-<!doctype html>
+        <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
+    <!-- Styles -->
+    <style>
 
-        .panel-body1 {
-          text-align:left;
-
-        }
-        .panel-body2 {
-          text-align:right;
-          float: right;
+        #block {
+            width: 50%;
+            height: 75%;
+            font-size: 25px;
+            float: left;
         }
 
-        </style>
-
+    </style>
 
 
 <body>
 @section('content')
-<div class="container">
-  <h1> Welcome to Dawson Friend Finder 2017 </h1>
-  <div class="navbar-header">
-    <!--Side bar nav-->
-    <ul class="nav navbar-nav">
-        @include('includes.header')
-    </ul>
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                </div>
+    <div class="container">
+        <h1> Welcome to Dawson Friend Finder 2017 </h1>
+        <div class="navbar-header">
+            <!--Side bar nav-->
+            <ul class="nav navbar-nav">
+                @include('includes.header')
+            </ul>
 
-                <div class="panel-body1">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+
+                    <div id="block">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <p> blah </p>
+                            </div>
+                            <div class="panel-body1">
+                                @if (session('status'))
+                                    <div class="alert alert-success">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                            </div>
                         </div>
-                    @endif
+                    </div>
 
-                    <p> blah </p>
-
-
-                </div>
-
-                <div class="panel-body2">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                    <div id="block">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <p> blah </p>
+                            </div>
+                            <div class="panel-body2">
+                                @if (session('status'))
+                                    <div class="alert alert-success">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                            </div>
                         </div>
-                    @endif
-
-                    <p> blah </p>
-
-
+                    </div>
                 </div>
-
 
             </div>
         </div>
-    </div>
-</div>
 @endsection
 
 </body>
