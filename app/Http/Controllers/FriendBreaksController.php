186ddd442b00c0c6bb3d0f44e5b6992077e3a98c
Kevin Bee <kevbui89@gmail.com>
2017-12-01 15:32:11 -0500

    added validation for fields

--- app/Http/Controllers/FriendBreaksController.php
+++ app/Http/Controllers/FriendBreaksController.php
@@ -13,11 +13,49 @@ use Auth;
 
 class FriendBreaksController extends Controller
 {
-    public function __construct() {
+    /**
+     * FriendBreaksController constructor.
+     */
+    public function __construct()
+    {
         $this->middleware('auth');
     }
 
-    public function index() {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
         return view('findFriendBreaks');
+    }
+
+    /**
+     * Checks if all the fields from the user are valid inputs
+     * @return bool
+     */
+    private function validateFields()
+    {
+        $valid = false;
+        if ($_POST['submitSearchBreaks']) {
+            if (isset($_POST['nameBreakSearch']) & isset($_POST['startTimeSearch']) & isset($_POST['endTimeSearch'])) {
+                if (!isEmpty($_POST['nameBreakSearch']) & !isEmpty($_POST['startTimeSearch']) & !isEmpty($_POST['endTimeSearch'])) {
+                    if (ctype_alpha($_POST['nameBreakSearch']) & is_numeric($_POST['startTimeSearch']) & is_numeric($_POST['endTimeSearch'])) {
+                        if (($_POST['startTimeSearch'] > 0 & $_POST['startTimeSearch'] < 2400) &
+                            ($_POST['endTimeSearch'] > $_POST['startTimeSearch'] & $_POST['endTimeSearch'] < 2400)) {
+                            $valid = true;
+                        } else {
+                            $valid = false;
+                        }
+                    } else {
+                        $valid = false;
+                    }
+                } else {
+                    $valid = false;
+                }
+            } else {
+                $valid = false;
+            }
+        }
+        return $valid;
     }
 }
