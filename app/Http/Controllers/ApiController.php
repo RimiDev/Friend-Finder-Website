19f511f9d01fca36bb37bb6b593bb051cdd4d8ac
Kevin Bee <kevbui89@gmail.com>
2017-12-06 18:00:16 -0500

    find friend api works, working on course api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,31 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\User_course;
 use Illuminate\Http\Request;
 use App\Course;
+use App\Friend;
+use App\User;
 use Auth;
-//use App\Friend;
 
 class ApiController extends Controller
 {
-    // TODO : changes required to database
 
     /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function findallfriendsbyuser(Request $request) {
+    public function allfriends(Request $request) {
         //check credentials
         $credentials = $request->only('email', 'password');
         $valid = Auth::once($credentials);
 
-        if (!valid) {
+        if (!$valid) {
             return response()->json(['error' => 'invalid_credentials'], 401);
         } else {
-            $friends = Friend::where('id', '=', Auth::user()->id)->get();
-            return response()->json($friends, 200);
+            $friendStatus = Friend::where(('email'), '=', Auth::user()->email)->get();
+
+            foreach ($friendStatus as $friend)
+                $friendNames[] = User::where('email', '=', $friend->friendEmail)->first();
+
+            $data = array();
+            if (isset($friendNames) && count($friendNames) > 0) {
+                foreach ($friendNames as $friends) {
+                    $data += ['email' => $friends->email, 'name' => $friends->name];
+                }
+            }
+
+            if (count($data) == 0) {
+                return response()->json(['email' => 'No friend found', 'name' => 'No friend found']);
+            }
+
+            return response()->json($data, 401);
         }
     }
+
+    public function coursefriends(Request $request) {
+        // check credentials
+        $credentials = $request->only('email', 'password');
+        $valid = Auth::once($credentials);
+
+        $coursename = $request->get('coursename');
+        $section = $request->get('section');
+
+        // Getting the friend object
+        $friendStatus = Friend::where(('email'), '=', Auth::user()->email)->get();
+
+        // Getting the user object of the friend
+        foreach ($friendStatus as $friend)
+            $friendNames[] = User::where('email', '=', $friend->friendEmail)->first();
+
+        // Array of email
+        $friendEmails= array();
+        foreach($friendNames as $f) {
+            $friendEmails[] = $f->email;
+        }
+
+        // Array of courses
+        $friendCourses = array();
+        for ($i = 0;$i < count($friendEmails); $i++) {
+            $friendCourses = User_course::where('email', '=', $friendEmails[$i])->first();
+        }
+
+        if (!$valid) {
+            return response()->json(['error' => 'invalid_credentials'], 401);
+        } else {
+            $data = array();
+            if (isset($friendCourses) && count($friendCourses) > 0) {
+                foreach ($friendCourses as $c) {
+                    $data += ['courseid' => $c];
+                }
+            }
+
+            return response()->json($data, 401);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      * @param Request $request
@@ -39,15 +108,10 @@ class ApiController extends Controller
         $credentials = $request->only('email', 'password');
         $valid = Auth::once($credentials);
 
-        if (!valid) {
+        if (!$valid) {
             return response()->json(['error' => 'invalid_credentials'], 401);
         } else {
-            // TODO : fetch the friends classes
-            // Need to change the db structure
-            $friendclass = array();
-            $classesid = array();
-            $friendclass = Course::where('courseNumber', '=', $coursename).get();
-            $classesid = Course::where('courseNumber', '=', $coursename).get();
+
         }
     }
 
@@ -63,11 +127,10 @@ class ApiController extends Controller
         $credentials = $request->only('email', 'password');
         $valid = Auth::once($credentials);
 
-        if (!valid) {
+        if (!$valid) {
             return response()->json(['error' => 'invalid_credentials'], 401);
         } else {
-            // TODO : fetch the friends breaks
-            // Modifications to tables needed
+
         }
     }
 
@@ -83,11 +146,10 @@ class ApiController extends Controller
         $credentials = $request->only('email', 'password');
         $valid = Auth::once($credentials);
 
-        if (!valid) {
+        if (!$valid) {
             return response()->json(['error' => 'invalid_credentials'], 401);
         } else {
-            // TODO : fetch friend location
-            // Modifications to tables needed
+
         }
     }
 }
